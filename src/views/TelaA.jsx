675026cff22e1b17cb686f42d190a460e97701f9
lndsilva <lndsilva@hotmail.com>
2024-03-31 19:53:29 -0300

    navegação stack tela 2

--- src/views/TelaA.jsx
+++ src/views/TelaA.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import TextoCentral from "../components/TextoCentral";
+import PassoStack from "../components/PassoStack";
 
-export default props =>(
-    <TextoCentral corFundo = '#e53935'>
-        Tela A
-    </TextoCentral>
-)
+export default props =>{
+    //props.navigation.navigate("TelaB")
+    return (
+        <PassoStack {...props} avancar="TelaB">
+            <TextoCentral corFundo = '#e53935'>
+                Tela A
+            </TextoCentral>
+        </PassoStack>
+    )
+}

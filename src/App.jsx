d7e104046a9df634003e354a5508f355861796ee
lndsilva <lndsilva@hotmail.com>
2024-03-31 18:37:25 -0300

    stak parte 1

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+import TelaA from "./views/TelaA";
+
+export default props => (
+    <SafeAreaView style={{flex: 1}}>
+        <TelaA/>
+    </SafeAreaView>
+)
